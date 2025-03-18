4036d8b95c8929707275c88b9201199097388f81
5akurasi <5akurasilegends@gmail.com>
2025-03-18 17:33:23 +0200

    Update wikka.bat

--- wikka.bat
+++ wikka.bat
@@ -19,8 +19,10 @@ if "%user_input%"=="/help command 1" (
     echo Команды Wikka страница 1
     echo.
     echo /color [цвет]
+    echo /bcolor [цвет фона]
     echo /help 
     echo /help color
+    echo /help bcolor
     echo /help command
     echo /exit
     echo /say
@@ -29,6 +31,7 @@ if "%user_input%"=="/help command 1" (
 )
 
 if "%user_input%"=="/exit" exit
+
 if "%user_input%"=="/color 0" color 0
 if "%user_input%"=="/color 1" color 1
 if "%user_input%"=="/color 2" color 2
@@ -46,8 +49,24 @@ if "%user_input%"=="/color d" color d
 if "%user_input%"=="/color e" color e
 if "%user_input%"=="/color f" color f
 
+if "%user_input%"=="/bcolor 0" set "bg_color=0" && call :change_bg_color
+if "%user_input%"=="/bcolor 1" set "bg_color=1" && call :change_bg_color
+if "%user_input%"=="/bcolor 2" set "bg_color=2" && call :change_bg_color
+if "%user_input%"=="/bcolor 3" set "bg_color=3" && call :change_bg_color
+if "%user_input%"=="/bcolor 4" set "bg_color=4" && call :change_bg_color
+if "%user_input%"=="/bcolor 5" set "bg_color=5" && call :change_bg_color
+if "%user_input%"=="/bcolor 6" set "bg_color=6" && call :change_bg_color
+if "%user_input%"=="/bcolor 7" set "bg_color=7" && call :change_bg_color
+if "%user_input%"=="/bcolor 8" set "bg_color=8" && call :change_bg_color
+if "%user_input%"=="/bcolor 9" set "bg_color=9" && call :change_bg_color
+if "%user_input%"=="/bcolor a" set "bg_color=a" && call :change_bg_color
+if "%user_input%"=="/bcolor b" set "bg_color=b" && call :change_bg_color
+if "%user_input%"=="/bcolor c" set "bg_color=c" && call :change_bg_color
+if "%user_input%"=="/bcolor d" set "bg_color=d" && call :change_bg_color
+if "%user_input%"=="/bcolor e" set "bg_color=e" && call :change_bg_color
+if "%user_input%"=="/bcolor f" set "bg_color=f" && call :change_bg_color
+
 if "%user_input%"=="/help color" (
-    echo.
     echo.
     echo Все Цвета терминала Wikka
     echo.
@@ -71,6 +90,30 @@ if "%user_input%"=="/help color" (
     echo.
 )
 
+if "%user_input%"=="/help bcolor" (
+    echo.
+    echo Все Цвета фона терминала Wikka
+    echo.
+    echo /bcolor 0 - Чёрный
+    echo /bcolor 1 - Синий
+    echo /bcolor 2 - Зелёный
+    echo /bcolor 3 - Голубой
+    echo /bcolor 4 - Красный
+    echo /bcolor 5 - Пурпурный
+    echo /bcolor 6 - Жёлтый
+    echo /bcolor 7 - Белый
+    echo /bcolor 8 - Серый
+    echo /bcolor 9 - Светло-синий
+    echo /bcolor a - Светло-зелёный
+    echo /bcolor b - Светло-голубой
+    echo /bcolor c - Светло-красный
+    echo /bcolor d - Светло-пурпурный
+    echo /bcolor e - Светло-жёлтый
+    echo /bcolor f - Светло-белый
+    echo.
+    echo.
+)
+
 if "%user_input%"=="/help" (
     echo.
     echo Что умеет Wikka?
@@ -88,4 +131,8 @@ if "%user_input%"=="/say" (
     echo !message!
 )
 
+goto WAIT
+
+:change_bg_color
+color %bg_color%
 goto WAIT
